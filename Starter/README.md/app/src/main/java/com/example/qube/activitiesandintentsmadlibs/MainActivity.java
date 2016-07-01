ee53bacc1ec52f3ccd118378fb2e2c5647f19db9
Jason Chan <jchan122490@gmail.com>
2016-07-01 09:03:37 -0400

    Jason having trouble with the onclicklistener will work more on it

---
+++ Starter/README.md/app/src/main/java/com/example/qube/activitiesandintentsmadlibs/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.qube.activitiesandintentsmadlibs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText mAdj1
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
